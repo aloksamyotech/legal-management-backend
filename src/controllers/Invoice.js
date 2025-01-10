66a982d0fdc6aaffd6fae208ba99f9440b91b35f
unknown <rahul.malviya@samyotech.com>
2025-01-10 11:07:26 +0530

    git comment resolve

--- src/controllers/Invoice.js
+++ src/controllers/Invoice.js
@@ -2,27 +2,27 @@ import * as invoiceService from "../services/invoice.js";
 import { Message, statusCodes } from "../core/common/constant.js";
 const InvoiceAdd = async (req, res, next) => {
   const InvoiceData = await invoiceService.AddInvoice(req, res, next);
-  res.status(statusCodes?.created).send(InvoiceData);
+  res.status(statusCodes.created).send(InvoiceData);
 };
 const InvoiceFetchByid = async (req, res, next) => {
   const InvoiceData = await invoiceService.GetInvoiceById(req, res, next);
-  res.status(statusCodes?.ok).send(InvoiceData);
+  res.status(statusCodes.ok).send(InvoiceData);
 };
 const AllInvoiceFetch = async (req, res, next) => {
   const InvoiceData = await invoiceService.GetInvoices(req, res, next);
-  res.status(statusCodes?.ok).send(InvoiceData);
+  res.status(statusCodes.ok).send(InvoiceData);
 };
 const InvoiceFetchByCase = async (req, res, next) => {
   const InvoiceData = await invoiceService.GetInvoiceByCaseId(req, res, next);
-  res.status(statusCodes?.ok).send(InvoiceData);
+  res.status(statusCodes.ok).send(InvoiceData);
 };
 const InvoiceDelete = async (req, res, next) => {
   const InvoiceDelData = await invoiceService.DeleteInvoice(req, res, next);
-  res.status(statusCodes?.ok).send(InvoiceDelData);
+  res.status(statusCodes.ok).send(InvoiceDelData);
 };
 const InvoiceUpdate = async (req, res, next) => {
   const InvoiceUpdateData = await invoiceService.UpdateInvoice(req, res, next);
-  res.status(statusCodes?.ok).send(InvoiceUpdateData);
+  res.status(statusCodes.ok).send(InvoiceUpdateData);
 };
 
 export default {
