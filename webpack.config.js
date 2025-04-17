1a2ee4c3b2e55a91a9951ef64996b365a90875e3
amansamyotech <aman.asati@samyotech,com>
2025-04-17 11:05:56 +0530

    complete the ci cd setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
+  mode: "production", 
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
